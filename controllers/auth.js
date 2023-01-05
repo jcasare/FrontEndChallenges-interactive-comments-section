d83fe49a0cafb852618bb972fc4ff64115bf5d4b
jcasare <asarejerry16@gmail.com>
2023-01-04 22:18:09 -0500

    work on login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,23 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+  //check to see that email and password are being sent to the server for authentication
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  //check db to confirm if user email exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  //check to see if password entered on client side matches db password
+  const isPasswordCorrect = user.comparePasswords(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const token = user.genToken();
+  res.cookie("token", { token, user: { name: user.name } }, { signed: true });
 };
 
 module.exports = { register, login };
